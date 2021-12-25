bac2e36873e8240eb0214b2a09f1fe04e24b1b46
kirill <kirillklopov8954408@gmail.com>
2021-12-25 17:07:31 +0300

    behaviorSubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MyPogoda';
+  rand:any 
+  timerId:any
+  ngOnInit(): void {
+    let subject = new BehaviorSubject(0);
+    subject.subscribe((x)=>console.log(x))
+  
+     this.timerId = window.setInterval(() => {
+     let a = Math.floor(Math.random()*1000)
+     subject.next(a)
+}, 2000);
+subject.subscribe((value:number)=>{
+  if(value >=300 && value<=700){
+    this.rand = value
+  }
+})
+  }
+onClick(){
+setTimeout(()=>{
+  clearInterval(this.timerId);
+},0)
 }
+}
+  
